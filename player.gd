228d9b360d0b66cafb51f658cd4eab6e8aac818c
sinamatic <84576780+sinamatic@users.noreply.github.com>
2025-01-02 22:58:04 +0100

    paddle bewegt sich immer noch nicht, aber osc_value ist jetzt wenigstens nicht mehr null

--- player.gd
+++ player.gd
@@ -2,39 +2,34 @@ extends StaticBody2D
 
 var win_height : int
 var p_height : int
-var osc_value : float = 0.0  # Initialer OSC-Wert
+var osc_value : float  # Aktueller OSC-Wert
 
 @onready var osc_receiver : Node = $OSCReceiver
 
 func _ready():
-	if osc_receiver:
-		print("OSCReceiver gefunden:", osc_receiver)
-		print("Zugehöriger Server:", osc_receiver.target_server)
-		print("OSC-Adresse:", osc_receiver.osc_address)
-	else:
-		print("Kein OSCReceiver gefunden")
+# 	if osc_receiver:
+# 		print("OSCReceiver gefunden:", osc_receiver)
+# 		print("Zugehöriger Server:", osc_receiver.target_server)
+# 		print("OSC-Adresse:", osc_receiver.osc_address)
+# 	else:
+# 		print("Kein OSCReceiver gefunden")
 
 	win_height = get_viewport_rect().size.y
 	p_height = $ColorRect.get_size().y
 
 func _process(delta):
+
+	# Überprüfe eingehende Nachrichten
 	if osc_receiver and osc_receiver.has_method("get"):
-		var target_server = osc_receiver.target_server
+		var target_server = $OSCServer
 		if target_server and target_server.incoming_messages.has(osc_receiver.osc_address):
 			osc_value = target_server.incoming_messages[osc_receiver.osc_address][0]
-			print("Empfangener OSC-Wert:", osc_value)
 			target_server.incoming_messages.erase(osc_receiver.osc_address)
+			
+			# Skaliere den OSC-Wert in den Bereich [-100, 100]
+		
+		# Begrenze die Paddle-Position
+		position.y = clamp(position.y, p_height / 2, win_height - p_height / 2)
 
-	# Bewege das Paddle
-	if osc_value != 0:
-		var movement = osc_value * delta * 500  # Teste mit fester Geschwindigkeit
-		print("OSC-Wert für Bewegung:", osc_value)
-		print("Berechnete Bewegung:", movement)
-
-		position.y -= movement  # Abhängig von der Richtung des OSC-Werts
-
-	# Begrenze die Paddle-Position
-	position.y = clamp(position.y, p_height / 2, win_height - p_height / 2)
-
-	# Debug-Ausgabe für Position
-	print("Aktuelle Paddle-Position:", position.y)
+		# Debugging-Ausgabe
+		print("Aktuelle Paddle-Position:", position.y, "OSC-Wert x 1000000: ", (osc_value * 10000))
