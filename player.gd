691029aaad2874b30c155c98dd4b6137e1337a60
sinamatic <84576780+sinamatic@users.noreply.github.com>
2025-01-02 23:23:51 +0100

    nicht perfekt aber man kann das linke paddle (player1) jetzt mit dem handy über OSC steuern

--- player.gd
+++ player.gd
@@ -7,29 +7,23 @@ var osc_value : float  # Aktueller OSC-Wert
 @onready var osc_receiver : Node = $OSCReceiver
 
 func _ready():
-# 	if osc_receiver:
-# 		print("OSCReceiver gefunden:", osc_receiver)
-# 		print("Zugehöriger Server:", osc_receiver.target_server)
-# 		print("OSC-Adresse:", osc_receiver.osc_address)
-# 	else:
-# 		print("Kein OSCReceiver gefunden")
-
 	win_height = get_viewport_rect().size.y
 	p_height = $ColorRect.get_size().y
 
 func _process(delta):
-
 	# Überprüfe eingehende Nachrichten
 	if osc_receiver and osc_receiver.has_method("get"):
 		var target_server = $OSCServer
 		if target_server and target_server.incoming_messages.has(osc_receiver.osc_address):
 			osc_value = target_server.incoming_messages[osc_receiver.osc_address][0]
 			target_server.incoming_messages.erase(osc_receiver.osc_address)
-			
-			# Skaliere den OSC-Wert in den Bereich [-100, 100]
-		
-		# Begrenze die Paddle-Position
-		position.y = clamp(position.y, p_height / 2, win_height - p_height / 2)
 
-		# Debugging-Ausgabe
-		print("Aktuelle Paddle-Position:", position.y, "OSC-Wert x 1000000: ", (osc_value * 10000))
+			# Skaliere den OSC-Wert auf den Bildschirmbereich
+			var normalized_value = clamp(osc_value, -1.0, 1.0)  # Normalisiere den Wert auf [-1, 1]
+			var target_y = win_height / 2.0 + normalized_value * (win_height / 2.0 - p_height / 2.0)
+			
+			# Bewege das Paddle sanft Richtung Zielposition
+			position.y = lerp(float(position.y), float(target_y), delta * 5.0)  # "5.0" ist die Glättungsrate
+
+			# Debugging-Ausgabe
+			print("Aktuelle Paddle-Position:", position.y, "Normalisierter Wert:", normalized_value)
